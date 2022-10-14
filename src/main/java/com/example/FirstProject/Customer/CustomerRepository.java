fc35c86d90bf20c5b84a8d4d325ce04c24263a05
Rahul Bhagwat <72269385+AutomationIsEverywhere@users.noreply.github.com>
2022-10-14 11:14:41 -0500

    Command Line Runner and a new Way of creating beans

--- src/main/java/com/example/FirstProject/Customer/CustomerRepository.java
+++ src/main/java/com/example/FirstProject/Customer/CustomerRepository.java
@@ -2,12 +2,12 @@ package com.example.FirstProject.Customer;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collections;
 import java.util.List;
 
-@Component
-@Primary
+
 public class CustomerRepository implements CustomerRepo
 {
     @Override
